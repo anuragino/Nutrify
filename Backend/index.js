5e4e8f4416e1bc121b225cbd9cf73549329069cb
Anurag Kumar <anuragino20@gmail.com>
2024-03-26 22:27:41 +0530

    updated vercel.js

--- Backend/index.js
+++ Backend/index.js
@@ -38,6 +38,13 @@ const app = express()
 // Middleware
 app.use(express.json())
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 
